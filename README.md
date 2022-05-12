61e60fed322be4a153d4ed7b5925069c400b7af5
404err-or <ssoh1738@kakao.com>
2022-05-12 15:16:02 +0900

    first final version commit

--- README.md
+++ README.md
@@ -5,7 +5,8 @@
 
 # Problem
 - [X] 1. socket.io에는 session이 없음. => 아무리 잘 해놓아봤자, 새로고침하면 초기화. 어떻게 할 것인가?
-- [ ] 2. db를 node 실행 초기 한번만 connection 함. => 계속된 연결이 있지 않는 한, 연결이 끊김. 어떻게 해결할까? 
+- [X] 2. db를 node 실행 초기 한번만 connection 함. => 계속된 연결이 있지 않는 한, 연결이 끊김. 어떻게 해결할까?
+  => mysql에 마지막 요청을 기준으로 wait_timeout의 시간이 경과하면 PROTOCOL_CONNECTION_LOST 오류 발생하며 connection 잃음.
 
 
 # Solution
@@ -18,46 +19,60 @@ const session_data = session({
     saveUninitialized: true
 });
 ``````
-like this.
-
+ 2. mysql connection 대신 mysql pool을 쓰면 된다더라..?
+  [related link](https://stackoverflow.com/questions/20210522/nodejs-mysql-error-connection-lost-the-server-closed-the-connection)
 
 # todo
 ## index.html
-- [ ] index.html 디자인
+- [X] index.html 디자인
 - [X] index.html socket => send message code
 - [X] index.html socket => receive message code
 - [X] index.html socket => receive message 시, 나인지 상대방인지 구별
 - [X] index.html socket => receive type code
-- [ ] 악성 값이 0이면 초록색 1이면 노란색 2 이상이면 빨간색
+- [X] 악성 값이 0이면 초록색 1이면 노란색 2 이상이면 빨간색
 - [X] 이름 변경 막는 코드
-
+- [X] 위로 스크롤 시, DB에서 추가 데이터 가져오기
 
 ## (etc).html
-- [ ] signin.html => 회원가입 페이지 완성하기
-- [ ] login.html => 로그인 페이지 완성하기
-- [ ] forget.html => 비밀번호 찾기 페이지 완성하기
+- [X] signin.html => 회원가입 페이지 완성하기
+- [X] login.html => 로그인 페이지 완성하기
+- [X] forget.html => 비밀번호 찾기 페이지 완성하기
 
 ## signin.html
-- [ ] 회원가입 닉네임 중복확인
-- [ ] 회원가입 이메일 중복확인
+- [X] 회원가입 닉네임 중복확인
+- [X] 회원가입 이메일 중복확인
 - [X] 회원가입 이메일 인증하기
+
+## /signin post
+- [X] sql 닉네임 중복확인
+- [X] sql 이메일 중복확인
+- [X] 이메일 인증코드 확인
+- [X] pw 유효한지, 같은지 확인
 
 ## server.js
 - [X] XXS 막기
 - [X] server.js socket => send message code
-- [ ] server.js socket => receive message code
-- [ ] receive message => DB에 넣기
-- [ ] server.js socket => receive type code
-- [ ] receive type => DB에 넣기
-- [ ] server.js DB => login code
-- [ ] server.js DB => logout code
-- [ ] server.js DB => signin code
-- [ ] server.js DB => signin 시, valid nickname & valid email 확인.
-- [ ] server.js DB => forget code
+- [X] server.js socket => receive message code
+- [X] receive message => DB에 넣기
+- [X] server.js socket => receive type code
+- [X] receive type => DB에 넣기
+- [X] server.js DB => login code
+- [X] server.js => logout code
+- [X] server.js DB => signin code
+- [X] server.js DB => signin 시, valid nickname & valid email 확인.
+- [X] server.js DB => forget code
 - [X] 이름 변경 막는 코드
 - [X] sql Object 완성하기
 - [X] sql injection 막기
 - [X] sql 사용하기.. => https://reddb.tistory.com/143
-- [ ] sql 적용하기
+- [X] sql 적용하기
 - [X] email 보내는거 테스트하기
 
+## forget.html
+ - nickname => email로 nickname 보내주기
+ - password => 인증 완료 시, 비밀번호 변경 가능하게.
+
+## 추가할 것.
+ - [ ] 추가 채팅방
+ - [ ] 이벤트(생일 이벤트, 복불복 등등)
+ - [ ] 급식 등등 키워드 모달창
--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const xss = require('xss'); // XSS 방지
 const session = require('express-session');
 const ios = require("express-socket.io-session");
 const axios = require("axios");
-const { sql_query, db } = require("./db");
-const { Auth, sendmail } = require("./email");
+const db = require("./db");
+const { Auth, sendmail, sendNameMail } = require("./email");
 require('dotenv').config();
 const session_data = session({
     secret: process.env.SESSION_SECRET,
@@ -20,25 +20,12 @@ const session_data = session({
 });
 
 const getHtml = async (text) => {
-  try {
-    return await axios.get("https://spocjaxkrk.execute-api.ap-northeast-2.amazonaws.com/v1/detect?comment="+encodeURIComponent(text));
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        return await axios.get("https://spocjaxkrk.execute-api.ap-northeast-2.amazonaws.com/v1/detect?comment="+encodeURIComponent(text));
+    } catch (error) {
+        console.error(error);
+    }
 };
-
-const sql = {
-    'insert_data': 'INSERT INTO re22_chat (nickname,message,date,type) values (?, ?, now(), ?)',
-    'insert_user': 'INSERT INTO re22_user (nickname, pw, email) values (?, password(?), ?)',
-    'insert_type': 'UPDATE re22_chat SET type=? where id=?',
-    'select_data': 'SELECT * FROM re22_chat WHERE ? <= id and id <= ?',
-    'valid_name': 'SELECT * FROM re22_user WHERE nickname=?',
-    'valid_email': 'SELECT * FROM re22_user WHERE email=?',
-    'valid_login': 'SELECT * FROM re22_user WHERE nickname=? and pw=password(?)'
-};
-
-sql_key = Object.keys(sql);
-console.log(sql_key);
 
 console.log(__dirname)
 app.set("view engine", "ejs");
@@ -54,24 +41,29 @@ io.use(ios(
     }
 ))
 
-let count=1;
-
 let email_verify = {};
 
 app.get("/",function(req, res){
-    // if(req.session.name) {
-        res.render("client", {name: req.session.name});
-    // } else {
-        //     res.redirect("/login");
-        // }
+    if(req.session.name) {
+        chat_before = chat_id>15?chat_id-15:0;
+        db.sql_query('select_data_by_id', [chat_before,chat_id])
+            .then(result => {
+                console.log(result);
+                for(let i in result) {
+                    result[i].date = result[i].date.toString().split(' ')[4];
+                }
+                res.render("client", {name: req.session.name, list:result});
+            })
+            .catch(result => {
+                console.log(result);
+                res.send('db error');
+            })
+    } else {
+        res.redirect("/login");
+    }
 });
-
-app.get('/session', function(req,res) {
-    req.session.name = "user" + count++;
-    res.send('success');
-})
     
-app.get("/signup", function(req,res){
+app.get("/login", function(req,res){ // login site
     if(req.session.name) {
         res.redirect("/");
     } else {
@@ -79,10 +71,56 @@ app.get("/signup", function(req,res){
     }
 });
 
-app.post("/signup", function(req,res){
+app.post("/login", function(req,res){ // login site
+    if(req.session.name) {
+        console.log(req.session)
+        res.redirect("/");
+    } else {
+        if(req.body && req.body.name && req.body.password) {
+            console.log(req.body);
+            db.sql_query('valid_login', [req.body.name, req.body.password])
+                .then(result => {
+                    if(result.length == 0) {
+                        res.send('<script>alert("로그인 실패!");history.go(-1)</script>');
+                    } else {
+                        req.session.name = req.body.name;
+                        res.redirect('/');
+                    }
+                })
+        } else {
+            res.redirect("/");
+        }
+    }
 });
 
-app.post('/check_email', function(req, res) {
+app.post("/add_content", function(req, res){
+    if(req.session.name) {
+        let id = req.body.id;
+        db.sql_query('select_data_by_id', [id - 16, id-1])
+            .then(result => {
+                console.log(result);
+                for(let i in result) {
+                    result[i].date = result[i].date.toString().split(' ')[4];
+                }
+                res.json(result.reverse());
+            })
+        .catch(result => {
+            console.log(result);
+            res.json(['error']);
+        })
+    } else {
+        res.json(['fail']);
+    }
+})
+
+app.get("/logout", function(req,res){ // login site
+    if(req.session.name) {
+        req.session.destroy();
+    }
+    res.redirect("/login");
+});
+
+app.post('/check_email', function(req, res) { // email authentication code
     let email = req.body.email;
     let code = Auth();
     let status = sendmail(code, email);
@@ -98,7 +136,7 @@ app.post('/check_email', function(req, res) {
     }
 });
 
-app.post('/verify_email', function(req, res) {
+app.post('/verify_email', function(req, res) { // email authentication code check
     let email = req.body.email;
     let code = req.body.code;
     if(email_verify[email] == code && email_verify[email] && email_verify[email].length == 29) {
@@ -108,9 +146,9 @@ app.post('/verify_email', function(req, res) {
     }
 });
 
-app.post('/same_name', function(req, res){
+app.post('/same_name', function(req, res){ // check if the name of the same exist
     let name = req.body.name;
-    sql_query('valid_name', [name])
+    db.sql_query('valid_name', [name])
         .then(result => {
             if(result) {
                 if(result.length == 0) {
@@ -127,9 +165,9 @@ app.post('/same_name', function(req, res){
         })
 });
 
-app.post('/same_email', function(req, res){
+app.post('/same_email', function(req, res){ // check if the email of the same exist
     let email = req.body.email;
-    sql_query('valid_email', [email])
+    db.sql_query('valid_email', [email])
         .then(result => {
             if(result) {
                 if(result.length == 0) {
@@ -146,17 +184,147 @@ app.post('/same_email', function(req, res){
         })
 });
 
-app.get("/signin", function(req,res){
+app.get("/signin", function(req,res){ // siginin site
     res.sendFile(__dirname + "/public/signin.html");
 });
 
+app.post("/signin", function(req,res){
+    let name = req.body.name;
+    let email = req.body.email;
+    let code = req.body.email_verify;
+    let pw = req.body.pw;
+    let valid_pw = req.body.valid_pw;
+    db.sql_query('valid_name', [name])
+        .then(result => {
+            if(result) {
+                if(result.length == 0) {
+                    return 'success';
+                } else if(result.length == 1) {
+                    throw new Error('name fail');
+                } else {
+                    console.log(result);
+                    throw new Error(`name error1 ${result}`);
+                }
+            } else {
+                throw new Error(`name error2 ${result}`);
+            }
+        })
+        .then(() => db.sql_query('valid_email', [email]))
+        .then(result => {
+            if(result) {
+                if(result.length == 0) {
+                    return 'success';
+                } else if(result.length == 1) {
+                    throw new Error('email fail');
+                } else {
+                    console.log(result);
+                    throw new Error(`email error1 ${result}`);
+                }
+            } else {
+                throw new Error(`email error2 ${result}`);
+            }
+        })
+        .then(() => {
+            if(email_verify[email] == code && email_verify[email] && email_verify[email].length == 29) {
+                return 'success';
+            } else {
+                throw new Error('verify email fail');
+            }
+        })
+        .then(() => {
+            if(pw === valid_pw && pw.length >= 8) {
+                return 'success';
+            } else {
+                throw new Error('pw fail');
+            }
+        })
+        .then(() => db.sql_query('insert_user', [name, pw, email]))
+        .then(() => res.send('success'))
+        .catch((err) => {
+            console.log(err.message);
+            res.send(err.message)
+        })
+    console.log(req.body);
+});
+
 app.get("/forget", function(req,res){
-    res.sendFile(__dirname + "/public/forget.html");
+    if(req.session.name) {
+        res.redirect('/');
+    } else {
+        res.sendFile(__dirname + "/public/forget.html");
+    }
+});
+
+app.post("/forget", function(req,res){
+    if(req.session.name) {
+        res.redirect('/');
+    } else {
+        if(req.body && req.body.type && req.body.email && req.body.email_verify) {
+            let type = req.body.type;
+            let email = req.body.email;
+            let code = req.body.email_verify;
+            if(type == 'NameTarget') {
+                if(email_verify[email] == code && email_verify[email] && email_verify[email].length == 29) {
+                    //  sql 통해 닉네임 찾기
+                    db.sql_query('valid_email', [email])
+                        .then(result => {
+                            console.log(result);
+                            if(result) {
+                                if(result.length == 1) {
+                                    return result[0].nickname;
+                                } else if(result.length == 0) {
+                                    throw new Error('email fail');
+                                } else {
+                                    console.log(result);
+                                    throw new Error(`email error1 ${result}`);
+                                }
+                            } else {
+                                throw new Error(`email error2 ${result}`);
+                            }
+                        })
+                        .then((result)=>sendNameMail(email, result))
+                        .then(()=>res.send('success'))
+                        .catch((err)=> {
+                            console.log(err);
+                            res.send('fail');
+                        })
+                } else {
+                    res.send('fail')
+                }
+            } else if(type == 'PassTarget') {
+                let pw = req.body.pw;
+                let valid_pw = req.body.valid_pw;
+                if(email_verify[email] == code && email_verify[email] && email_verify[email].length == 29) {
+                    if(pw === valid_pw && pw.length >= 8) {
+                        db.sql_query('change_pw', [pw,email])
+                            .then(result => res.send('success'))
+                            .catch((err)=> {
+                                console.log(err);
+                                res.send('error');
+                            })
+                    } else {
+                        res.send('fail');
+                    }
+                } else {
+                    res.send('fail');
+                }
+            }
+        }
+    }
 });
 
 let chat_id = 0;
+let chat_before = 0;
+db.sql_query('count_chat',[])
+    .then(result => {
+        if(result.length == 0) {
+            chat_id = 0;
+        } else {
+            chat_id = result[0]['id'];
+        }
+    })
 
-// db.query('select id from re22_chat order by id desc limit 1;', function (err, rows, fields) {
+// db.db.query('select id from re22_chat order by id desc limit 1;', function (err, rows, fields) {
 //     if (!err) {
 //         if(rows == []) {
 //             chat_id = 0;
@@ -168,14 +336,12 @@ let chat_id = 0;
 //         console.log(err);
 //     }
 // });
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
     console.log('user connected: ', socket.id);
     let name;
     console.log(socket.handshake.session.name);
     if(socket.handshake.session.name == undefined) {
-        name = "user" + count++;
-        socket.handshake.session.name = name;
-        socket.handshake.session.save();
+        socket.disconnect(true);
     } else {
         name = socket.handshake.session.name;
     }
@@ -187,13 +353,14 @@ io.on('connection', function(socket){
     
     socket.on('send message', function(name,text){
         let chk_name = socket.handshake.session.name;
+        let time = new Date().toString().split(' ')[4];
         if(name == chk_name) {
             chat_id++;
             if(text.trim() !== ''){
                 let id = chat_id;
                 let req_text = xss(text);
-                // sql_query('insert_data', [name, req_text, -1]);
-                io.emit('receive message', name, req_text, id);
+                io.emit('receive message', name, req_text, id, time);
+                db.sql_query('insert_data', [name, req_text, -1]);
                 let msg = name + ':' + req_text;
                 console.log(id + '.' + msg);
                 getHtml(req_text)
@@ -201,7 +368,7 @@ io.on('connection', function(socket){
                     .then(type => {
                         console.log(id, msg, type);
                         io.emit('receive type', type, id);
-                        // sql_query('insert_type', [type, id]);
+                        db.sql_query('insert_type', [type, id]);
                     })
                     .catch(err => console.log(err))
             }
@@ -216,6 +383,8 @@ http.listen('3000', function(){
 });
 
 process.on('exit', (code)=>{
+    let t = new Date();
+    console.log(t.toString());
     console.log(`exit code : ${code}`);
     if(code !== 0) {
         logger.error({
@@ -224,10 +393,11 @@ process.on('exit', (code)=>{
             timestamp: new Date(),
         });
     }
+    db.db.end();
 });
 
 process.once('SIGINT', ()=>{
-    db.destroy();
+    db.db.end();
     console.log("You've pressed Ctrl + C on this process.");
     process.exit();
 })
